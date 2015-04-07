68d795dfbd02f75711359936aca017b972bbcb63
Phil Brockwell <nineteen40eight@gmail.com>
2015-04-07 09:53:55 +0100

    refactored using rubocop

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -6,6 +6,6 @@ class WeatherHolder; include Weather; end
 describe Weather do
   it 'knows if the weather is sunny or not' do
     weather = WeatherHolder.new
-    expect(weather.sunny?).to satisfy { |weather| true || false }
+    expect(weather.sunny?).to satisfy { true || false }
   end
 end
